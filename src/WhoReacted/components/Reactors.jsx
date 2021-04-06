125fcf756a51e6e467ee93a4124d2098d2b631b8
FichteFoll <fichtefoll2@googlemail.com>
2021-04-07 00:00:47 +0200

    fix(WhoReacted): adjust count in more users

--- src/WhoReacted/components/Reactors.jsx
+++ src/WhoReacted/components/Reactors.jsx
@@ -6,20 +6,22 @@ const ReactionStore = WebpackModules.getByProps('getReactions', '_changeCallback
 const VoiceUserSummaryItem = WebpackModules.find(m => m?.default?.displayName === 'VoiceUserSummaryItem').default;
 
 const Reactors = ({ count, max, users, userId, showBot, size }) => {
+    const filteredUsers = users.filter(user => user.id != userId && (showBot || user.bot !== true));
+    const filtered = users.length - filteredUsers.length;
+
     function renderMoreUsers(text, className) {
         return (
             <div className={`${className} bd-who-reacted-more-reactors`}>
-                +{1 + count - max}
+                +{1 + count - max - filtered}
             </div>
         );
     }
 
-    users = users.filter(user => user.id != userId && (showBot || user.bot !== true));
     return (
         <VoiceUserSummaryItem
             className={`bd-who-reacted-reactors bd-who-reacted-size-${size}px`}
             max={max}
-            users={users}
+            users={filteredUsers}
             renderMoreUsers={renderMoreUsers}
         />
     );
