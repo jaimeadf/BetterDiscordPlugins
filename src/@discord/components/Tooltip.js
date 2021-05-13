8bd8cf6d44931e8924954e8b7609ba75b217b567
Marmota <jaimeadanielfilho@hotmail.com>
2021-05-13 20:27:53 -0300

    style: format destructuring

--- src/@discord/components/Tooltip.js
+++ src/@discord/components/Tooltip.js
@@ -1,8 +1,12 @@
 import { WebpackModules } from '@zlibrary/api';
 
-const { TooltipColors, TooltipContainer, TooltipLayer, TooltipPositions, default: Tooltip } = WebpackModules.getByProps(
-    'TooltipContainer'
-);
+const {
+    TooltipColors,
+    TooltipContainer,
+    TooltipLayer,
+    TooltipPositions,
+    default: Tooltip
+} = WebpackModules.getByProps('TooltipContainer');
 
 export { TooltipColors, TooltipContainer, TooltipLayer, TooltipPositions };
 
