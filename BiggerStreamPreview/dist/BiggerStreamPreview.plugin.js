f93ab6a36c0aa7434a6e0f6a518333584b143201
jaimeadf <jaimeadanielfilho@hotmail.com>
2023-02-12 17:19:41 +0000

    chore: build 122f648101b590c30e992f17980ccaec6d3d6f92

--- BiggerStreamPreview/dist/BiggerStreamPreview.plugin.js
+++ BiggerStreamPreview/dist/BiggerStreamPreview.plugin.js
@@ -53,128 +53,116 @@ __webpack_require__.d(__webpack_exports__, {
 const external_BdApi_React_namespaceObject = global["BdApi"]["React"];
 var external_BdApi_React_default = /*#__PURE__*/__webpack_require__.n(external_BdApi_React_namespaceObject);
 ;// CONCATENATED MODULE: ./src/index.jsx
-function _defineProperty(obj, key, value) {
-    if (key in obj) {
-        Object.defineProperty(obj, key, {
-            value: value,
-            enumerable: true,
-            configurable: true,
-            writable: true
-        });
-    } else {
-        obj[key] = value;
-    }
-    return obj;
-}
-function _extends() {
-    _extends = Object.assign || function(target) {
-        for(var i = 1; i < arguments.length; i++){
-            var source = arguments[i];
-            for(var key in source){
-                if (Object.prototype.hasOwnProperty.call(source, key)) {
-                    target[key] = source[key];
-                }
-            }
-        }
-        return target;
-    };
-    return _extends.apply(this, arguments);
-}
 
-const { Webpack , ContextMenu  } = BdApi;
-const { Filters  } = Webpack;
-const openModal = Webpack.getModule(Filters.byStrings("modalKey", "Layer", "onCloseCallback"), {
-    searchExports: true
-});
-const ModalRoot = Webpack.getModule(Filters.byStrings("impressionType", "MODAL"), {
-    searchExports: true
-});
-const ImageModal = Webpack.getModule(Filters.byStrings("renderMobileCloseButton"), {
-    searchExports: true
-});
-const ModalSize = Webpack.getModule(Filters.byProps("DYNAMIC"), {
-    searchExports: true
-});
-const MaskedLink = Webpack.getModule((m)=>m?.type?.toString()?.includes("MASKED_LINK"), {
-    searchExports: true
-});
-const useStateFromStores = Webpack.getModule(Filters.byStrings("useStateFromStores"), {
-    searchExports: true
-});
+
+const { Webpack, ContextMenu } = BdApi;
+const { Filters } = Webpack;
+
+const openModal = Webpack.getModule(Filters.byStrings("modalKey", "Layer", "onCloseCallback"), { searchExports: true });
+
+const ModalRoot = Webpack.getModule(Filters.byStrings("impressionType", "MODAL"), { searchExports: true });
+const ImageModal = Webpack.getModule(Filters.byStrings("renderMobileCloseButton"), { searchExports: true });
+const ModalSize = Webpack.getModule(Filters.byProps("DYNAMIC"), { searchExports: true });
+
+const MaskedLink = Webpack.getModule(m => m?.type?.toString()?.includes("MASKED_LINK"), { searchExports: true });
+
+const useStateFromStores = Webpack.getModule(Filters.byStrings("useStateFromStores"), { searchExports: true });
+
 const StreamStore = Webpack.getModule(Filters.byProps("getStreamForUser"));
 const StreamPreviewStore = Webpack.getModule(Filters.byProps("getPreviewURL"));
+
 const imageModalStyles = Webpack.getModule(Filters.byProps("modal", "image"));
+
 class BiggerStreamPreview {
     start() {
         this.patchUserContextMenu();
         this.patchStreamContextMenu();
     }
+
     stop() {
         this.unpatchUserContextMenu();
         this.unpatchStreamContextMenu();
     }
+
     patchUserContextMenu() {
         ContextMenu.patch("user-context", this.handleUserContextMenu);
     }
+
     unpatchUserContextMenu() {
         ContextMenu.unpatch("user-context", this.handleUserContextMenu);
     }
+
     patchStreamContextMenu() {
         ContextMenu.patch("stream-context", this.handleStreamContextMenu);
     }
+
     unpatchStreamContextMenu() {
         ContextMenu.unpatch("stream-context", this.handleStreamContextMenu);
     }
-    appendStreamPreviewMenuGroup(menu, previewUrl) {
-        menu.props.children.splice(menu.props.children.length - 1, 0, this.buildStreamPreviewMenuGroup(previewUrl));
-    }
-    buildStreamPreviewMenuGroup(previewUrl) {
-        return /*#__PURE__*/ external_BdApi_React_default().createElement(ContextMenu.Group, null, /*#__PURE__*/ external_BdApi_React_default().createElement(ContextMenu.Item, {
-            id: "stream-preview",
-            label: "View Stream Preview",
-            action: ()=>this.openImageModal(previewUrl),
-            disabled: !previewUrl
-        }));
-    }
-    openImageModal(previewUrl) {
-        openModal((props)=>/*#__PURE__*/ external_BdApi_React_default().createElement(ModalRoot, _extends({
-                className: imageModalStyles.modal,
-                size: ModalSize.DYNAMIC
-            }, props), /*#__PURE__*/ external_BdApi_React_default().createElement(ImageModal, {
-                className: imageModalStyles.image,
-                shouldAnimate: true,
-                original: previewUrl,
-                placeholder: previewUrl,
-                src: previewUrl,
-                renderLinkComponent: (props)=>/*#__PURE__*/ external_BdApi_React_default().createElement(MaskedLink, _extends({}, props))
-            })));
-    }
-    constructor(){
-        _defineProperty(this, "handleUserContextMenu", (menu, { user  })=>{
-            const [stream, previewUrl] = useStateFromStores([
-                StreamStore,
-                StreamPreviewStore
-            ], ()=>{
-                const stream = StreamStore.getStreamForUser(user.id);
-                const previewUrl = stream && StreamPreviewStore.getPreviewURL(stream.guildId, stream.channelId, stream.ownerId);
-                return [
-                    stream,
-                    previewUrl
-                ];
-            });
-            if (stream) {
-                this.appendStreamPreviewMenuGroup(menu, previewUrl);
-            }
-        });
-        _defineProperty(this, "handleStreamContextMenu", (menu, { stream  })=>{
-            const previewUrl = useStateFromStores([
-                StreamPreviewStore
-            ], ()=>StreamPreviewStore.getPreviewURL(stream.guildId, stream.channelId, stream.ownerId));
-            this.appendStreamPreviewMenuGroup(menu.props.children, previewUrl);
-        });
-    }
-}
 
+    appendStreamPreviewMenuGroup(menu, previewUrl) {
+        menu.props.children.splice(
+            menu.props.children.length - 1,
+            0,
+            this.buildStreamPreviewMenuGroup(previewUrl)
+        );
+    }
+
+    buildStreamPreviewMenuGroup(previewUrl) {
+        return (
+            external_BdApi_React_default().createElement(ContextMenu.Group, null
+                , external_BdApi_React_default().createElement(ContextMenu.Item, {
+                    id: "stream-preview",
+                    label: "View Stream Preview"  ,
+                    action: () => this.openImageModal(previewUrl),
+                    disabled: !previewUrl,}
+                )
+            )
+        );
+    }
+
+    openImageModal(previewUrl) {
+        openModal(props => (
+            external_BdApi_React_default().createElement(ModalRoot, { className: imageModalStyles.modal, size: ModalSize.DYNAMIC, ...props,}
+                , external_BdApi_React_default().createElement(ImageModal, {
+                    className: imageModalStyles.image,
+                    shouldAnimate: true,
+                    original: previewUrl,
+                    placeholder: previewUrl,
+                    src: previewUrl,
+                    renderLinkComponent: props => external_BdApi_React_default().createElement(MaskedLink, { ...props,} ),}
+                )
+            )
+        ));
+    }
+
+    handleUserContextMenu = (menu, { user }) => {
+        const [stream, previewUrl] = useStateFromStores([StreamStore, StreamPreviewStore], () => {
+            const stream = StreamStore.getStreamForUser(user.id);
+            const previewUrl = stream && StreamPreviewStore.getPreviewURL(
+                stream.guildId,
+                stream.channelId,
+                stream.ownerId
+            );
+
+            return [stream, previewUrl];
+        });
+
+        if (stream) {
+            this.appendStreamPreviewMenuGroup(menu, previewUrl);
+        }
+    };
+
+    handleStreamContextMenu = (menu, { stream }) => {
+        const previewUrl = useStateFromStores([StreamPreviewStore], () => StreamPreviewStore.getPreviewURL(
+            stream.guildId,
+            stream.channelId,
+            stream.ownerId
+        ));
+
+        this.appendStreamPreviewMenuGroup(menu.props.children, previewUrl);
+    };
+}
 
 module.exports = __webpack_exports__["default"];
 /******/ })()
